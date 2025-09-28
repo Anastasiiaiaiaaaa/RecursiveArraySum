c9a1dc501a9e294799e9736e267bd9684e2c9871
Anastasiia Sokolova <nastiasokolova.od.ua@gmail.com>
2025-09-28 22:05:55 +0300

    sumArray

--- RecursiveArraySum/RecursiveArraySum.cpp
+++ RecursiveArraySum/RecursiveArraySum.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+int sumArray(int arr[], int size);
 int main()
 {
-    std::cout << "Hello World!\n";
+    const int n = 5;
+    int arr[n] = { 1, 2, 3, 4, 5 };
+    std::cout << "Sum of array elements: " << sumArray(arr, n) << std::endl;
+}
+int sumArray(int arr[], int size) {
+    if (size == 0) return 0;
+    return arr[size - 1] + sumArray(arr, size - 1);
 }
